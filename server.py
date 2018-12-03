22e51b521924b1e7889908fc7ca66cd5fc6466d5
Panhao Wu <wupanhao@qq.com>
2018-12-03 15:34:31 +0000

    auto length of recording

--- server.py
+++ server.py
@@ -3,16 +3,21 @@ import sys
 import signal
 from stt2 import *
 import os
+from record import Mic
 
 interrupted = False
 token = get_token()
-
+mic = Mic()
+mic.fetchThreshold()
+detector = None
 def detected_callback():
         global detector
+        global mic
         detector.terminate()
         print "hotword detected"
         os.system("aplay resources/ding.wav")
-        os.system("arecord -d 4 -r 16000 -c 1 -t wav -f S16_LE record.wav")
+        # os.system("arecord -d 4 -r 16000 -c 1 -t wav -f S16_LE record.wav")
+        mic.activeListenToAllOptions()
         use_cloud(token)
         detector.start(detected_callback=detected_callback,
                interrupt_check=interrupt_callback,
@@ -22,27 +27,33 @@ def signal_handler(signal, frame):
     global interrupted
     interrupted = True
 
-
 def interrupt_callback():
     global interrupted
     return interrupted
 
-if len(sys.argv) == 1:
-    print("Error: need to specify model name")
-    print("Usage: python demo.py your.model")
-    sys.exit(-1)
+def main():
+  global detector
+  if len(sys.argv) == 1:
+      print("Error: need to specify model name")
+      print("Usage: python demo.py your.model")
+      sys.exit(-1)
 
-model = sys.argv[1]
+  print(len(sys.argv))
+  model = sys.argv[1]
 
-# capture SIGINT signal, e.g., Ctrl+C
-signal.signal(signal.SIGINT, signal_handler)
+  # capture SIGINT signal, e.g., Ctrl+C
+  signal.signal(signal.SIGINT, signal_handler)
 
-detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
-print('Listening... Press Ctrl+C to exit')
+  detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
+  print('Listening... Press Ctrl+C to exit')
 
-# main loop
-detector.start(detected_callback=detected_callback,
-               interrupt_check=interrupt_callback,
-               sleep_time=0.03)
+  # main loop
+  detector.start(detected_callback=detected_callback,
+                 interrupt_check=interrupt_callback,
+                 sleep_time=0.03)
 
-detector.terminate()
+  detector.terminate()
+
+
+if __name__ == '__main__':
+  main()
